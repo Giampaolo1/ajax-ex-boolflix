181f49021389ba1c7929e2fd9ea42b6e55bbc013
Giampaolo1 <giampaolo.r@outlook.com>
2019-11-26 11:19:43 +0100

    MS 2 Start

--- script/main.js
+++ script/main.js
@@ -19,10 +19,7 @@ $(document).ready(function() {
     $(".container").html("");
 
     var ricercaUtente = $(".barraRicerca").val();
-    console.log(ricercaUtente);
-
-    // ripulisco la barretta
-    // $(".barraRicerca").val("");
+    // console.log(ricercaUtente);
 
     // al click del bottone parte la chiamata ajax x la ricerca
 
@@ -52,23 +49,22 @@ $(document).ready(function() {
 
 
       success: function(data){
-        console.log(data)
+        // console.log(data)
 
         // HB
         var source = $("#entry-template").html();
         var template = Handlebars.compile(source);
         // HB
 
-
         for (var i = 0; i < data.results.length; i++) {
           var oggetto = data.results[i];
+
+          $('.container').append(html)
 
           // HB
           var context = oggetto;
           var html = template(context);
           // HB
-
-          $('.container').append(html)
 
           // USA CICLO FOR..IN X CICLARE gli OGGETTI (pensato apposta)
 
@@ -81,8 +77,6 @@ $(document).ready(function() {
           // Inserisco nell'html con append
           // STAMPA IN PAGINA
           // $('.container').append(oggetto)
-
-
 
           // console.log(i + " " + data.results[i].title);
           // console.log(oggetto.title);
@@ -97,7 +91,20 @@ $(document).ready(function() {
           // console.log(oggetto.vote_average);
 
           // console.log("-------");
+
+          // 4.vote_average
+          // Trasformiamo il numero da 1 a 10 decimale in un numero intero da 1 a 5
+          console.log(oggetto.vote_average);
+          console.log("--------");
+          var arrotonda = (oggetto.vote_average/2)
+          console.log(arrotonda);
+
+
+
         }
+
+      // ripulisco la barra ricerca x una nuova ricerca
+      $(".barraRicerca").val("");
 
     },
       error: function(){
@@ -107,6 +114,29 @@ $(document).ready(function() {
 
   });
 
+  // 4.vote_average
+  // Trasformiamo il numero da 1 a 10 decimale in un numero intero da 1 a 5
+
 
 
 });
+
+// Milestone 2:
+// Trasformiamo il numero da 1 a 10 decimale in un numero intero da 1 a 5, così da permetterci di stampare
+// a schermo un numero di stelle piene che vanno da 1 a 5, lasciando le restanti vuote
+// (troviamo le icone in FontAwesome).
+// Arrotondiamo sempre per eccesso all’unità successiva, non gestiamo icone mezze piene.
+
+// Trasformiamo poi la stringa statica della lingua in una vera e propria bandiera della nazione corrispondente,
+// gestendo il caso in cui non abbiamo la bandiera della nazione ritornata dall’API
+// (le flag non ci sono in FontAwesome).
+//
+// Allarghiamo poi la ricerca anche alle serie tv. Con la stessa azione di ricerca dovremo prendere sia i film
+// che corrispondono alla query, sia le serie tv, stando attenti ad avere alla fine dei valori simili
+// (le serie e i film hanno campi nel JSON di risposta diversi, simili ma non sempre identici)
+
+// Qui un esempio di chiamata per le serie tv:
+// https://api.themoviedb.org/3/search/tv?api_key=e99307154c6dfb0b4750f6603256716d&language=it_IT&query=scrubs
+
+
+// IDEA: controllo sui grassetti
